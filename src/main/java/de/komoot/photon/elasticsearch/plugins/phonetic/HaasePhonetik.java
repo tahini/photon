9ef33a83f2cb7e243d02a6c3238843d118c9fceb
nicolae.diana <nicolae.diana@courrier.uqam.ca>
2022-05-31 15:20:31 -0400

    ajouté licences

--- src/main/java/de/komoot/photon/elasticsearch/plugins/phonetic/HaasePhonetik.java
+++ src/main/java/de/komoot/photon/elasticsearch/plugins/phonetic/HaasePhonetik.java
@@ -1,5 +1,29 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
 package de.komoot.photon.elasticsearch.plugins.phonetic;
 
+/**
+ * Ge&auml;nderter Algorithmus aus der Matching Toolbox von Rainer Schnell
+ * Java-Programmierung von J&ouml;rg Reiher
+ *
+ * Die Kölner Phonetik wurde für den Einsatz in Namensdatenbanken wie
+ * der Verwaltung eines Krankenhauses durch Martin Haase (Institut für
+ * Sprachwissenschaft, Universität zu Köln) und Kai Heitmann (Insitut für
+ * medizinische Statistik, Informatik und Epidemiologie, Köln)  überarbeitet.
+ * M. Haase und K. Heitmann. Die Erweiterte Kölner Phonetik. 526, 2000.
+ *
+ * nach: Martin Wilz, Aspekte der Kodierung phonetischer Ähnlichkeiten
+ * in deutschen Eigennamen, Magisterarbeit.
+ * http://www.uni-koeln.de/phil-fak/phonetik/Lehre/MA-Arbeiten/magister_wilz.pdf
+ *
+ * @author <a href="mailto:joergprante@gmail.com">J&ouml;rg Prante</a>
+ */
 public class HaasePhonetik extends KoelnerPhonetik {
 
     private static final String[] HAASE_VARIATIONS_PATTERNS = {
--- src/main/java/de/komoot/photon/elasticsearch/plugins/phonetic/PhoneticTokenFilterFactory.java
+++ src/main/java/de/komoot/photon/elasticsearch/plugins/phonetic/PhoneticTokenFilterFactory.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
 package de.komoot.photon.elasticsearch.plugins.phonetic;
 
 import org.apache.commons.codec.Encoder;
@@ -22,9 +30,6 @@ import org.elasticsearch.env.Environment;
 import org.elasticsearch.index.IndexSettings;
 import org.elasticsearch.index.analysis.AbstractTokenFilterFactory;
 import org.elasticsearch.index.analysis.TokenFilterFactory;
-
-
-
 
 import java.util.HashSet;
 import java.util.List;
